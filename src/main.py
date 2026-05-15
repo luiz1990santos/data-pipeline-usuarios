4707295f806acfbf7c2303e611c38f3f97cedfee
Luiz <luiz_santos1990@hotmail.com>
2026-05-15 18:23:37 -0300

    refactor: inclusao da etapa transform no script main

--- src/main.py
+++ src/main.py
@@ -1,21 +1,25 @@
 from extract import conexao_api_clientes
 from load import importar_json_clientes, atualizar_processados
-#from transform import import_json_normalizado
-from helpers import caminho_lista_processados
+from transform import transformacao_clientes
+# from helpers import caminho_lista_processados
 
 
-clientes= conexao_api_clientes()
+raw = conexao_api_clientes()
+
+import_clientes = None
+
+if raw and len(raw.get("results", [])) > 0:
+     import_clientes = importar_json_clientes(raw)
+
+# print(import_clientes)
+
+transformacao_clientes(import_clientes)
 
 
-#print(api['results'])
-
-if clientes and len(clientes.get("results", [])) > 0:
-     import_clientes = importar_json_clientes(clientes)
-
-# normalizado = import_json_normalizado()
 
 
 arquivos_gerados = atualizar_processados()
+
 
 
 
