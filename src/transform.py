8ff2d722f9aa4df17f6588d16642b47ef3b0078f
Luiz <luiz_santos1990@hotmail.com>
2026-05-15 18:29:37 -0300

    feat: implementação do script transform

---
+++ src/transform.py
@@ -0,0 +1,72 @@
+import pandas as pd
+import json
+from helpers import caminho_bronze_clientes, data_arquivo
+
+
+data_arquivo = data_arquivo()
+
+caminho_bronze = caminho_bronze_clientes()
+
+def transformacao_clientes(arquivo):
+
+    caminho = str(caminho_bronze)
+
+    nome_arquivo = str(data_arquivo)
+
+    # print(data_arquivo)
+
+    with open(arquivo, "r", encoding="utf-8") as a:
+        dados = json.load(a)
+
+    usuarios = dados.get("results", [])
+
+    usuarios_transformados = []
+
+    for usuario in usuarios:
+        usuario_transformado = {
+            "id": usuario.get("login", {}).get("uuid"),
+            "first_name": usuario.get("name", {}).get("first"),
+            "last_name": usuario.get("name", {}).get("last"),
+            "gender": usuario.get("gender"),
+            "email": usuario.get("email"),
+            "street": usuario.get("location", {}).get("street", {}).get("name"),
+            "number": usuario.get("location", {}).get("street", {}).get("number"),
+            "city": usuario.get("location", {}).get("city"),
+            "state": usuario.get("location", {}).get("state"),
+            "country": usuario.get("location", {}).get("country"),
+            "latitude": usuario.get("location", {}).get("coordinates", {}).get("latitude"),
+            "longitude": usuario.get("location", {}).get("coordinates", {}).get("longitude"),
+            "date_of_birth": usuario.get("dob", {}).get("date"),
+            "age": usuario.get("dob", {}).get("age"),
+            "registration_date": usuario.get("registered", {}).get("date"),
+            "regist_age": usuario.get("registered", {}).get("age")
+        }
+
+        usuarios_transformados.append(usuario_transformado)
+
+    json_normalizado = pd.json_normalize(usuarios_transformados)
+
+    arquivo_saida = f"{caminho}/{nome_arquivo}-clientes.csv"
+
+    json_normalizado.to_csv(arquivo_saida, index = False)
+
+
+    return json_normalizado
+
+
+
+# teste = transformacao_clientes()
+
+# print(teste)
+
+# dataset = pd.json_normalize(teste)
+
+
+# print(dataset.dtypes)
+
+
+
+# print(dataset.describe())
+
+
+# csv = dataset.to_csv
