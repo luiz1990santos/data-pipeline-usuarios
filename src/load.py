75fc7dac9807136cded1305b49f2db184b34fb3e
Luiz <luiz_santos1990@hotmail.com>
2026-05-24 17:00:32 -0300

    chore: reparo no código

--- src/load.py
+++ src/load.py
@@ -15,12 +15,12 @@ def importar_json_clientes(dados):
 
     # print(data_formatada)
     try:
-        caminho = f'{cm_clientes}/{dt}-clientes.json'
+        caminho = f'{cm_clientes}/{dt}-users.json'
 
         with open(caminho, "w", encoding="utf-8") as f:
             json.dump(dados, f, indent=4, ensure_ascii=False)
         
-        log("INFO", f"Arquivo {dt}_clientes.json criado com Sucesso", "load")
+        log("INFO", f"Arquivo {dt}-users.json criado com Sucesso", "load")
     except Exception as e:
         log("ERROR", f"erro inesperado: {type(e).__name__}, {e}", "load")
 
@@ -62,40 +62,3 @@ def atualizar_processados():
 
 
 # print(lista_arquivos())
-
-
-
-"""
-def transformacao_clientes(cm_clientes):
-
-    with open(cm_clientes, "r", encoding="utf-8") as arquivo:
-        dados = json.load(arquivo)
-
-    usuarios = dados.get("results", [])
-
-    usuarios_transformados = []
-
-    for usuario in usuarios:
-        usuario_transformado = {
-            "id": usuario.get("login", {}).get("uuid"),
-            "first_name": usuario.get("name", {}).get("first"),
-            "last_name": usuario.get("name", {}).get("last"),
-            "gender": usuario.get("gender"),
-            "email": usuario.get("email"),
-            "street": usuario.get("street", {}).get("name"),
-            "number": usuario.get("street", {}).get("number"),
-            "city": usuario.get("location", {}).get("city"),
-            "state": usuario.get("location", {}).get("state"),
-            "country": usuario.get("location", {}).get("country"),
-            "latitude": usuario.get("coordinates", {}).get("latitude"),
-            "longitude": usuario.get("coordinates", {}).get("longitude"),
-            "date_of_birth": usuario.get("dob", {}).get("date"),
-            "age": usuario.get("dob", {}).get("age"),
-            "registration_date": usuario.get("registered", {}).get("date"),
-            "regist_age": usuario.get("registered", {}).get("age")
-        }
-
-        usuarios_transformados.append(usuario_transformado)
-
-    return usuarios_transformados
-"""
