ea22765692dfe250d558818457050fa3699922b0
Luiz <luiz_santos1990@hotmail.com>
2026-05-24 16:53:28 -0300

    chore: ajuste

--- sql/dml/insert_users.sql
+++ sql/dml/insert_users.sql
@@ -2,5 +2,15 @@
 -----------------------------------------
 -- TESTES INSERT TB - STAGING_USERS     |
 -----------------------------------------
-INSERT INTO STAGING_USERS ( ID, first_name,	last_name, gender,	email, street, number, city, state, country, latitude, longitude, date_of_birth, age, registration_date, regist_age)
-VALUES ('a0d9ee5c-be39-4c1b-9877-fbd4c272c33e','Manel','Barbosa','male','manel.barbosa@example.com','Rua Um','6157','Uberaba','Goiás','Brazil','63.3443','-169.6210','1985-01-29T09:10:59.163Z','41','2019-03-02T15:23:00.609Z','7');
+/*
+    INSERT INTO STAGING_USERS ( ID, first_name,	last_name, gender,	email, street, number, city, state, country, latitude, longitude, date_of_birth, age, registration_date, regist_age)
+    VALUES ('a0d9ee5c-be39-4c1b-9877-fbd4c272c33e','Manel','Barbosa','male','manel.barbosa@example.com','Rua Um','6157','Uberaba','Goiás','Brazil','63.3443','-169.6210','1985-01-29T09:10:59.163Z','41','2019-03-02T15:23:00.609Z','7');
+*/
+
+
+
+-- DROP TABLE STAGING_USERS
+
+
+
+
