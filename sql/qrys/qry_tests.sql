b946d1cc6947fa0e4acef98d7673bc4ed2468daf
Luiz <luiz_santos1990@hotmail.com>
2026-07-27 18:59:47 -0300

    chore: validacao de duplicidades nas tabelas

--- sql/qrys/qry_tests.sql
+++ sql/qrys/qry_tests.sql
@@ -1,1 +1,39 @@
-SELECT * FROM STAGING_USERS;
+
+USE ANALYTICS_HUB;
+
+SELECT COUNT(*) AS VOLUME_STAGING FROM STAGING_USERS;
+
+SELECT COUNT(*) AS VOLUME_SIVLER FROM SILVER_USERS;
+
+SELECT COUNT(*) AS VOLUME_GOLD FROM GOLD_USERS;
+
+-- TESTES DE DUPLICIDADE
+SELECT 
+    ID, 
+    count(*) as duplicidados 
+FROM STAGING_USERS
+GROUP BY ID
+HAVING count(*) > 1
+--ORDER BY created_at desc
+;
+
+
+
+SELECT 
+    ID, 
+    count(*) as duplicidados 
+FROM SILVER_USERS
+GROUP BY ID
+HAVING count(*) > 1
+--ORDER BY created_at desc
+;
+
+
+
+SELECT 
+    ID_CLIENTE, 
+    count(*) as duplicidados 
+FROM GOLD_USERS
+GROUP BY ID_CLIENTE
+HAVING count(*) > 1
+;
