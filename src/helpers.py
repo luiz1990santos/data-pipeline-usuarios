a45b262e027f75125d28221c0ac7d000d0b3834e
Luiz <luiz_santos1990@hotmail.com>
2026-07-28 10:02:44 -0300

    fix: reducao de codigo repetido

--- src/helpers.py
+++ src/helpers.py
@@ -11,9 +11,10 @@ from uuid import uuid4
 """
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+timestamp_int = int(datetime.datetime.now().timestamp())
+
 
 def caminho_raw_clientes():
-
     caminho = BASE_DIR / "data" / "raw"
     caminho.mkdir(parents=True, exist_ok=True)
 
@@ -21,7 +22,6 @@ def caminho_raw_clientes():
 
 
 def caminho_bronze_clientes():
-
     caminho = BASE_DIR / "data" / "bronze"
     caminho.mkdir(parents=True, exist_ok=True)
 
@@ -29,7 +29,6 @@ def caminho_bronze_clientes():
 
 
 def caminho_log():
-
     caminho = BASE_DIR / "log" 
     caminho.mkdir(parents=True, exist_ok=True)
 
@@ -37,7 +36,6 @@ def caminho_log():
 
 
 def caminho_lista_processados():
-    
     caminho = BASE_DIR / "data" / "pipeline_state"
     caminho.mkdir(parents=True, exist_ok=True)
 
@@ -45,20 +43,12 @@ def caminho_lista_processados():
 
 
 def data_arquivo():
-    # 1. Obtém o timestamp atual como inteiro
-    timestamp_int = int(datetime.datetime.now().timestamp())
-
-    # 2. Converte o inteiro para a data no formato solicitado
     data_formatada = datetime.datetime.fromtimestamp(timestamp_int).strftime('%Y-%m-%dT%H-%M-%S')
 
     return data_formatada 
 
 
 def data_registro():
-    # 1. Obtém o timestamp atual como inteiro
-    timestamp_int = int(datetime.datetime.now().timestamp())
-
-    # 1. Obtém o timestamp atual como inteiro
     data_atual = datetime.datetime.fromtimestamp(timestamp_int).strftime('%Y-%m-%d %H:%M:%S')
 
     return data_atual
