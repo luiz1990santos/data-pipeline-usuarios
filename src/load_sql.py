72cfbbf112333102419fb180082600a2dfd53e63
Luiz <luiz_santos1990@hotmail.com>
2026-07-29 11:58:18 -0300

    refactor: inclusão de logs e tratamentos, alteracao para olhar o ultimo run_id da execucao

--- src/load_sql.py
+++ src/load_sql.py
@@ -2,11 +2,18 @@ import os
 import pyodbc
 import pandas as pd
 from dotenv import load_dotenv
+import logging
 
+logger = logging.getLogger(__name__)
 
 def insert_db(csv): 
+    logging.info('Acesso ao DB')
     df = pd.read_csv(fr'{csv}')
     # print(df)
+
+    run_id = df['run_id'].drop_duplicates()[0]
+
+    # print(run_id)
 
     load_dotenv()
 
@@ -34,6 +41,7 @@ def insert_db(csv):
                                     last_name, 
                                     gender,	
                                     email, 
+                                    cpf,
                                     street, 
                                     number, 
                                     city, 
@@ -47,70 +55,97 @@ def insert_db(csv):
                                     regist_age, 
                                     created_at)
                        
-        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
                        
     """, data)
 
     sql_merge =  f"""
-                    WITH ORIGEM_FILTRADA AS (
-                        SELECT *,
-                            -- Cria um ranking para cada ID. O número 1 será o registro mais recente (ou o primeiro encontrado)
-                            ROW_NUMBER() OVER (PARTITION BY id ORDER BY created_at DESC) as RN
-                        FROM STAGING_USERS
-                        WHERE RUN_ID = (SELECT MAX(RUN_ID) FROM STAGING_USERS)
-                    )
-                    MERGE SILVER_USERS AS DESTINO 
-                    -- Agora usamos a CTE filtrada como origem, pegando apenas o registro único (RN = 1)
-                    USING (SELECT * FROM ORIGEM_FILTRADA WHERE RN = 1) AS ORIGEM
-                    ON (DESTINO.ID = ORIGEM.ID)
+                       WITH ORIGEM_FILTRADA AS (
+                            SELECT *,
+                                -- Cria um ranking para cada ID. O número 1 será o registro mais recente (ou o primeiro encontrado)
+                                ROW_NUMBER() OVER (PARTITION BY id ORDER BY created_at DESC) as RN
+                            FROM STAGING_USERS
+                            WHERE RUN_ID = ?
+                        ) 
+                        MERGE SILVER_USERS AS DESTINO 
+                        -- Agora usamos a CTE filtrada como origem, pegando apenas o registro único (RN = 1)
+                        USING ( SELECT * FROM ORIGEM_FILTRADA
+                                WHERE RN = 1) AS ORIGEM
+                        ON (DESTINO.ID = ORIGEM.ID)
 
-                    WHEN NOT MATCHED THEN 
-                        INSERT( run_id,
-                                id, 	
-                                first_name,	
-                                last_name,	
-                                gender,	
-                                email,	
-                                street,	
-                                number,	
-                                city,	
-                                state,	
-                                country,	
-                                latitude,	
-                                longitude,	
-                                date_of_birth,	
-                                age,	
-                                registration_date,	
-                                regist_age,
-                                created_at  )
-                        
-                        VALUES( ORIGEM.run_id,
-                                ORIGEM.id, 	
-                                ORIGEM.first_name,	
-                                ORIGEM.last_name,	
-                                ORIGEM.gender,	
-                                ORIGEM.email,	
-                                ORIGEM.street,	
-                                ORIGEM.number,	
-                                ORIGEM.city,	
-                                ORIGEM.state,	
-                                ORIGEM.country,	
-                                ORIGEM.latitude,	
-                                ORIGEM.longitude,	
-                                ORIGEM.date_of_birth,	
-                                ORIGEM.age,	
-                                ORIGEM.registration_date,	
-                                ORIGEM.regist_age,
-                                ORIGEM.created_at   )
-                                
-                    ;
+                        WHEN NOT MATCHED THEN 
+                            INSERT( run_id,
+                                    id, 	
+                                    first_name,	
+                                    last_name,	
+                                    gender,	
+                                    email,
+                                    cpf,	
+                                    street,	
+                                    number,	
+                                    city,	
+                                    state,	
+                                    country,	
+                                    latitude,	
+                                    longitude,	
+                                    date_of_birth,	
+                                    age,	
+                                    registration_date,	
+                                    regist_age,
+                                    created_at  )
+                            
+                            VALUES( ORIGEM.run_id,
+                                    ORIGEM.id, 	
+                                    ORIGEM.first_name,	
+                                    ORIGEM.last_name,	
+                                    ORIGEM.gender,	
+                                    ORIGEM.email,
+                                    ORIGEM.cpf,	
+                                    ORIGEM.street,	
+                                    ORIGEM.number,	
+                                    ORIGEM.city,	
+                                    ORIGEM.state,	
+                                    ORIGEM.country,	
+                                    ORIGEM.latitude,	
+                                    ORIGEM.longitude,	
+                                    ORIGEM.date_of_birth,	
+                                    ORIGEM.age,	
+                                    ORIGEM.registration_date,	
+                                    ORIGEM.regist_age,
+                                    TRY_CONVERT(DATETIME2(0), ORIGEM.created_at, 120)   )
+                                    
+                        ;
 
     """
 
     try:
-        cursor.execute(sql_merge)
+
+        cursor.execute(sql_merge, run_id)
         conexao.commit()
 
+        qry_staging = f'SELECT COUNT(*) FROM STAGING_USERS WHERE RUN_ID = ?;'
+
+        cursor.execute(qry_staging, run_id)
+        registros_staging = cursor.fetchall()[0][0]
+
+
+        qry_silver = f'SELECT COUNT(*) FROM SILVER_USERS WHERE RUN_ID = ?;'
+
+        cursor.execute(qry_silver, run_id)
+        registros_silver = cursor.fetchall()[0][0]
+
+        logging.info('Insert STAGING_USERS - Registros: %d', registros_staging)
+        logging.info('Insert SILVER_USERS - Registros: %d', registros_silver)
+
+
     except Exception as erro:
+        logging.error(f"erro inesperado: {type(erro).__name__}") 
         conexao.rollback()
         raise erro
+
+
+
+
+
+
+
