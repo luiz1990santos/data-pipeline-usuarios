04aa26a886b26ab6f9a92fe1b541714b00803bbd
Luiz <luiz_santos1990@hotmail.com>
2026-07-29 11:53:22 -0300

    refactor: incluindo inicio da chamada dos logs

--- src/main.py
+++ src/main.py
@@ -1,32 +1,45 @@
+import logging
+from logging_config import configurar_logging
 from extract import conexao_api_clientes
 from load import importar_json_clientes, atualizar_processados
 from transform import transformacao_clientes
 from load_sql import insert_db
 # from helpers import caminho_lista_processados
 
+logger = logging.getLogger(__name__)
 
-raw = conexao_api_clientes()
+def main():
+     configurar_logging()
 
-import_clientes = importar_json_clientes(raw)
-
-
-if raw and len(raw.get("results", [])) > 0:
-     import_clientes 
-
-# print(import_clientes)
-
-insert = transformacao_clientes(import_clientes)
+     logging.info('Pipeline iniciado')
 
 
-insert_db(insert)
+     raw = conexao_api_clientes()
 
-# insert_db(r"C:\Users\luiz_\OneDrive\Desktop\Engenharia_de_DadosV2\01-Projetos-2026\data-pipeline-usuarios\data\bronze\2026-07-27T18-01-07-users.csv")
-
-
-arquivos_gerados = atualizar_processados()
+     import_clientes = importar_json_clientes(raw)
 
 
+     if raw and len(raw.get("results", [])) > 0:
+          import_clientes 
 
+     # print(import_clientes)
+
+     insert = transformacao_clientes(import_clientes)
+
+
+     insert_db(insert)
+
+
+     # TESTE DE REPROCESSAMENTO
+     #insert_db(r"C:\Users\luiz_\OneDrive\Desktop\Engenharia_de_DadosV2\01-Projetos-2026\data-pipeline-usuarios\data\bronze\2026-07-27T20-53-22-users.csv")
+
+
+     arquivos_gerados = atualizar_processados()
+
+     logging.info('Status: Sucesso')
+
+if __name__ == "__main__":
+     main()
 
 
 
